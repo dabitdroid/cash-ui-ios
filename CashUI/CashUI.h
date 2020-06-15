adefa5353c72dc8452181c1a369c11dc109bd973
Giancarlo Pacheco <g1an@hotmail.com>
2020-06-15 11:22:23 -0400

    Initial Commit

---
+++ CashUI/CashUI.h
@@ -0,0 +1,19 @@
+//
+//  CashUI.h
+//  CashUI
+//
+//  Created by Giancarlo Pacheco on 6/15/20.
+//  Copyright © 2020 Giancarlo Pacheco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CashUI.
+FOUNDATION_EXPORT double CashUIVersionNumber;
+
+//! Project version string for CashUI.
+FOUNDATION_EXPORT const unsigned char CashUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CashUI/PublicHeader.h>
+
+
