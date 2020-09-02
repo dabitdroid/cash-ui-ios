5bfe6b09a24c5aba4b39ff1d210131c4c9974e7e
Gardner von Holt <gardner@wvh.com>
2020-09-02 08:09:10 -0500

    Removed override to light mode map and now rely on the global setting in the plist

--- Sources/CashUI/UI/OtherUI/WithdrawalStatusViewController.swift
+++ Sources/CashUI/UI/OtherUI/WithdrawalStatusViewController.swift
@@ -6,7 +6,7 @@ import CashCore
 private let kAtmAnnotationViewReusableIdentifier = "kAtmAnnotationViewReusableIdentifier"
 
 class WithdrawalStatusViewController: ActionViewController {
-    
+
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var atmMapView: MKMapView!
     @IBOutlet weak var atmLocationDescription: UILabel!
@@ -14,67 +14,63 @@ class WithdrawalStatusViewController: ActionViewController {
     @IBOutlet weak var amountBTCLabel: UILabel!
     @IBOutlet weak var addressTitleLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
-    
+
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var qrCodeImageView: UIImageView!
     @IBOutlet weak var redeemCodeLabel: UILabel!
     @IBOutlet weak var sendButton: UIButton!
-    
+
     var transaction: CoreTransaction!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         initialData()
         setMapLocation()
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(transactionDidUpdate), name: .CoreTransactionDidChange, object: nil)
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         update()
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     func initialData() {
         atmMapView.register(AtmAnnotationView.self,
                          forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         atmMapView.delegate = self
-        
-        if #available(iOS 13.0, *) {
-            atmMapView.overrideUserInterfaceStyle = .light
-        }
-        
+
         if let atm = transaction.atm, let latitude = atm.latitude,
             let longitude = atm.longitude {
             let atmLocation = CLLocation(latitude: (latitude as NSString).doubleValue, longitude: (longitude as NSString).doubleValue)
             atmMapView.centerToLocation(atmLocation)
             self.atmLocationDescription.text = transaction.atm?.addressDesc
         }
-        
+
         if let code = transaction.code {
             let btcAmount = (code.btcAmount! as NSString).doubleValue
             let usdAmount = (code.usdAmount! as NSString).doubleValue
-            
+
             setQRCode(from:code.address!, amount:btcAmount)
-        
+
             self.amountUSDLabel.text = "$\(usdAmount)"
             self.amountBTCLabel.text = "\(btcAmount) BTC"
             self.addressLabel.text = code.address
         }
     }
-    
+
     private func update() {
         if let code = transaction.pCode {
             self.redeemCodeLabel.text = code
         }
         self.updateStatus(transaction.status)
     }
-    
+
     func updateStatus(_ status: CoreTransactionStatus) {
         self.qrCodeImageView.isHidden = true
         self.redeemCodeLabel.isHidden = false
@@ -108,7 +104,7 @@ class WithdrawalStatusViewController: ActionViewController {
             break
         }
     }
-    
+
     private func setQRCode(from address: String, amount btc: Double) {
         let finalAddress = "bitcoin:\(address)?amount=\(btc)"
         guard let data = finalAddress.data(using: .utf8) else { return }
@@ -116,24 +112,24 @@ class WithdrawalStatusViewController: ActionViewController {
             .qrCode(data: data)!
             .resize(self.qrCodeImageView.frame.size)
     }
-    
+
     private func setMapLocation() {
         let annotation = AtmAnnotation.init(atm: transaction.atm!)
         self.atmMapView.addAnnotation(annotation)
     }
-    
+
     @objc override public func hideView() {
         super.hideView()
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     @objc func transactionDidUpdate(_ notification: Notification) {
         let t = notification.object as! CoreTransaction
         if (t == self.transaction) {
             update()
         }
     }
-    
+
     @IBAction func sendCoin(_ sender: Any) {
         AtmLocationsViewController.sendCoin(amount: (transaction.code?.btcAmount)!, address: (transaction.code?.address)!, completion: {
             CoreTransactionManager.updateTransaction(status: .Awaiting, address: (self.transaction.code?.address)!)
@@ -143,20 +139,20 @@ class WithdrawalStatusViewController: ActionViewController {
 }
 
 extension WithdrawalStatusViewController: MKMapViewDelegate {
-    
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            
+
             if annotation is MKUserLocation { return nil }
-            
+
             var annotationView = atmMapView.dequeueReusableAnnotationView(withIdentifier: kAtmAnnotationViewReusableIdentifier)
-            
+
             if annotationView == nil {
                 annotationView = AtmAnnotationView(annotation: annotation, reuseIdentifier: kAtmAnnotationViewReusableIdentifier)
                 annotationView?.image = UIImage(named: "atmWhite")
             } else {
                 annotationView!.annotation = annotation
             }
-            
+
             return annotationView
         }
 }
